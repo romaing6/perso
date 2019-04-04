b961cc9f787b7f032df8ad1baefddd8bc679da21
romaing6 <romain.gambinir@gmail.com>
2019-04-04 14:13:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-ascbc6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-ascbc6)
---
+++ index.js
@@ -0,0 +1,27 @@
+// Import stylesheets
+import './style.css';
+
+carto.setDefaultAuth({
+    username: 'romaing6',
+    apiKey: 'a508a5fcde7a2ef52caab5fc031f43e96a0204cc'
+});
+
+const map = new mapboxgl.Map({
+      container: 'map',
+      style: carto.basemaps.darkmatter,
+      center: [35, -6],
+      zoom: 4
+    });
+
+const source = new carto.source.SQL("SELECT A.*,B.status_group FROM pump_it_up_data_mining_the_water_table_training_set_values A left join pump_it_up_data_mining_the_water_table_training_set_labels B on B.id = A.id");
+
+
+const viz = new carto.Viz(`
+    color: ramp($status_group, [#66cc85, #f67171, #f8c374])
+    strokeWidth:0
+    width: ramp(zoomRange([10,15]),[1,scaled($population/2,15)])
+`);
+
+const layer = new carto.Layer('layer', source, viz);
+
+layer.addTo(map);
